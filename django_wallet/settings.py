f3c98e064232f1e1fe6a704cdb40e36728d49a11
MetaMaxfield <metamaxfield@gmail.com>
2025-08-13 15:00:20 +0400

    chore(auth): configure custom user model
    Set AUTH_USER_MODEL to use custom User model from users app:
    - Updated settings.py to point to 'users.User'
    - Prepares authentication system for future extensions
    - Maintains compatibility with Django's auth framework

--- django_wallet/settings.py
+++ django_wallet/settings.py
@@ -139,3 +139,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# User model
+
+AUTH_USER_MODEL = "users.User"
